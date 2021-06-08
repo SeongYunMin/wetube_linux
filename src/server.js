64016f6613a5fc835cc28f968427a9f41a7e7559
S.Y.Min <prioritymin@gmail.com>
2021-06-08 23:55:47 +0200

    0608_midnight_update

--- src/server.js
+++ src/server.js
@@ -4,6 +4,16 @@ const PORT = 4000;
 
 const app = express();
 
+// const handleHome = () => console.log("response from server");
+const handleHome = (req, res) => {
+  return res.send("<h1>Server Response!!</h1>");
+};
+const handleLogin = (req, res) => {
+  return res.send("Login here");
+};
+app.get("/", handleHome);
+app.get("/login", handleLogin);
+
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT} 🎃`);
 
