f249b71505aedaaacc49cd666e8ee078b08ae352
S.Y.Min <prioritymin@gmail.com>
2021-06-13 23:51:32 +0200

    0613_midnight_update

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,11 +1,8 @@
-export const trending = (req, res) => res.send("Trending Videos");
+export const trending = (req, res) => res.render("home");
 export const search = (req, res) => res.send("Search");
-export const see = (req, res) => {
-  res.send(`Watch video #${req.params.id}`);
-};
-export const edit = (req, res) => {
-  res.send(`edit video #${req.params.id}`);
-};
+export const see = (req, res) => res.render("watch");
+export const edit = (req, res) => res.render("edit");
+
 export const deleteVideo = (req, res) => {
   res.send(`delete video #${req.params.id}`);
 };
