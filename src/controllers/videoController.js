67eb6fc02feff8b66124177043f174af293b3b4e
S.Y.Min <prioritymin@gmail.com>
2021-06-14 23:35:47 +0200

    0614_midnight_update

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,9 +1,13 @@
-export const trending = (req, res) => res.render("home");
-export const search = (req, res) => res.send("Search");
-export const see = (req, res) => res.render("watch");
-export const edit = (req, res) => res.render("edit");
-
-export const deleteVideo = (req, res) => {
-  res.send(`delete video #${req.params.id}`);
+const fakeUser = {
+  username: "Nicolas",
+  loggedIn: true,
 };
+
+export const trending = (req, res) =>
+  res.render("home", { pageTitle: "Home", fakeUser });
+export const search = (req, res) => res.send("Search");
+export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
+export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });
+
+export const deleteVideo = (req, res) => res.send("Delete Video");
 export const upload = (req, res) => res.send("Upload Video");
