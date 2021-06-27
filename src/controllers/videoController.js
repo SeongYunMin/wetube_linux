c4f56ef045fb009ae8fd3c01f931b746b299ae98
SYMin <prioritymin@gmail.com>
2021-06-27 23:38:10 +0300

    0627_midnight_update

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,19 +1,12 @@
 import Video from "../models/Video";
 
-export const home = (req, res) => {
-  console.log("Start");
-  Video.find({}, (error, videos) => {
-    console.log("errors", error);
-    console.log("videos", videos);
-    console.log("db loading finished");
-    return res.render("home", { pageTitle: "Home", videos: [] });
-  });
-  console.log("I finished first");
+export const home = async (req, res) => {
+  const videos = await Video.find({});
+  return res.render("home", { pageTitle: "Home", videos });
 };
 
 export const watch = (req, res) => {
   const { id } = req.params;
-  console.log(`Show video #${id}`);
   res.render("watch", { pageTitle: `Watching` });
 };
 
--- src/init.js
+++ src/init.js
@@ -2,7 +2,7 @@ import "./db";
 import Video from "./models/Video";
 import app from "./server";
 
-const PORT = 5000;
+const PORT = 4000;
 
 const handleListening = () =>
   console.log(`✅ Server listening on port http://localhost:${PORT} 🎃`);
