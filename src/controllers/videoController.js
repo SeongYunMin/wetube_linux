5b202b6f2cb18a2f464a3ca4fa128e73847089be
SYMin <prioritymin@gmail.com>
2021-07-13 23:50:03 +0200

    0713_midnight_update

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -28,15 +28,21 @@ export const getUpload = (req, res) => {
 export const postUpload = async (req, res) => {
   console.log(req.body);
   const { title, description, hashtags } = req.body;
-  await Video.create({
-    title,
-    description,
-    createdAt: Date.now(),
-    hashtags: hashtags.split(",").map((word) => `#${word}`),
-    meta: {
-      views: 0,
-      rating: 0,
-    },
-  });
-  return res.redirect("/");
+  try {
+    await Video.create({
+      title,
+      description,
+      hashtags: hashtags.split(",").map((word) => `#${word}`),
+      meta: {
+        views: 0,
+        rating: 0,
+      },
+    });
+    return res.redirect("/");
+  } catch (error) {
+    return res.render("upload", {
+      pageTitle: "Uploading Video",
+      errorMessage: error._message,
+    });
+  }
 };
